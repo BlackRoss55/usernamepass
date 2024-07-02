e6067987d5bf460f568593b8e270eea8b250dd58
xx98burak98xx <burak_ahmet_10@hotmail.com>
2024-07-03 01:20:33 +0300

    proje tamamlandi

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,23 @@ public class Main {
         System.out.print("Şifrenizi giriniz: ");
         password = sc.nextLine();
 
-        if (userName.equals(userName2) && password.equals(password2)){
+        if (userName.equals(userName2) && password.equals(password2)) {
             System.out.println("Giriş başarılı.");
 
-        }
-        else {
+        } else {
             System.out.println("Kullanıcı adı veya şifre yanlış\nŞifrenizi sıfırlamak ister misiniz?\n1)Evet\n2)Hayır");
             int secim = sc.nextInt();
             switch (secim) {
                 case 1:
-                    System.out.print("Yeni şifrenizi giriniz:");
-                    password2 = sc.nextLine();
+                    System.out.println("Yeni şifrenizi giriniz:");
+                    password = sc.next();
+                    if(password.equals(password2)){
+                        System.out.println("Yeni şifre eski şifreyle aynı olamaz");
+
+                    }
+                    else {
+                        System.out.println("Şifre başarıyla değiştirildi");
+                    }
                     break;
                 case 2:
                     System.out.println("Şifre değiştirilmedi. Giriş başarısız.");
